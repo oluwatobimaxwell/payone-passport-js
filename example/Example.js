10053311f1321049be31784603c693ca61656b6a
oluwatobimaxwell <39818137+oluwatobimaxwell@users.noreply.github.com>
2021-07-17 13:55:39 +0100

    Update Example.js

--- example/Example.js
+++ example/Example.js
@@ -9,8 +9,8 @@ export const Example = () => {
     React.useEffect(() => {
         setstatus("Processing...")
         const img = document.createElement("img")
-        img.src = require("./images/side-person.jpg").default;
-
+        img.src = require("./images/passport.jpg").default;
+        document.getElementById("input-image").src = img.src;
         const config = { input: img, MODEL_URL: "/models" };
         new Passport(config)
         .then(res => {
@@ -26,8 +26,17 @@ export const Example = () => {
 
     return (
         <div>
-            {result && (<img id="result-image" src={result} alt={"Output image"} />)}
-            <p>{status}</p>
+            <div  style={{ display: "flex" }}>
+            <div>
+                <p style={{ textAlign: "center" }}>Input Image</p>
+            <img id="input-image" alt={"Input image"} style={{ width: 500, height: "auto" }} />
+            </div>
+            <div>
+                <p style={{ textAlign: "center"}}>Output Image</p>
+            {result && (<img id="result-image" src={result} alt={"Output image"} style={{ marginLeft: 50 }} />)}
+            </div>
+            </div>
+            <p style={{ padding: 20, border: '2px solid', marginTop: 20, width: "max-content" }}><b>Status: </b> {status}</p>
         </div>
     )
 }
