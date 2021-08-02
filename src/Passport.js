7041f24ee5f3236d4f5cf79932a0c9a63b55563c
oluwatobimaxwell <39818137+oluwatobimaxwell@users.noreply.github.com>
2021-08-02 16:55:18 +0100

    Update Passport.js
    Validation constraints updated

--- src/Passport.js
+++ src/Passport.js
@@ -1,4 +1,10 @@
 import * as faceapi from 'face-api.js';
+
+const constraints = {
+    width: 384,
+    height: 384
+}
+
 class Passport {
     constructor({ input, showLandMark, MODEL_URL }){
         this.input = input;
@@ -11,7 +17,7 @@ class Passport {
     run =  async () => {
         await this.loadModels();
         await this.detectFaces();
-        this.getFaceParts();
+        await this.getFaceParts();
         const result = this.results();
         return new Promise((resolve, reject) => {
             if(result?.status){
@@ -49,16 +55,19 @@ class Passport {
     }
 
     getPassportDimens = (faceWidth, faceHeight, posX, posY) => {
+
+
         let width = faceWidth/0.53125;
         let height =  width/0.75;
-        const dX = width/2 - faceWidth/2;
+        const dX = height/2 - faceWidth/2;
         const dY = height/2 - faceHeight/2;
         const x = posX - dX;
         const y = posY - dY;
-        return { width, height, x, y }
+
+        return { width: height, height, x, y }
     }
 
-    getFaceParts = () => {
+    getFaceParts = async () => {
         const marks = this.oneface;
         const unshift = marks?.unshiftedLandmarks;
         const landmarks = marks?.landmarks;
@@ -84,38 +93,98 @@ class Passport {
         // Box 
         const align = marks?.alignedRect;
         this.detectScore = align?._score;
-        const size = align?._imageDims;
-
-        const box = align?._box;
         const ctx = this.canvas.getContext('2d');
-        // ctx.drawImage(this.input, box._x, box._y, size?._width, size?._height);
-        console.log("Data: ", box)
-        console.log("Size: ", size)
-
         const { _imgDims, _shift } = landmarks;
 
-        const passport = this.getPassportDimens(_imgDims._width, _imgDims._height, _shift._x, _shift._y)
-        
-        //
-        ctx.canvas.width  = passport.width;
-        ctx.canvas.height = passport.height;
-        this.imageDimension = {
-            width: passport.width,
-            height: passport.height
-        }
 
-        ctx.drawImage(
-                        this.input, 
-                        passport.x, passport.y, passport.width, passport.height,
-                        0, 0, passport.width, passport.height
-                    );
-        ctx.canvas.style.backgroundColor = '#fff'
-        this.image = this.canvas.toDataURL('image/jpeg', 1.0)
+        // this.image = this.canvas.toDataURL('image/jpeg', 1.0);
+        const sampleBg = await this.getBGSample(this.input, _imgDims._width);
+        this.image = sampleBg?.src;
+        this.backgroundColor = this.getAverageRGB(sampleBg);
+        
         this.runChecks();
+        // Get background sample
+    }
+
+     bgColorWhiteCheck = (color) => {
+        const brightness = ((color.r * 299) + (color.g * 587) + (color.b * 114)) / 1000;
+        return brightness > 225;
+    }
+
+    getBGSample = (image, faceWidth) => {
+        let sampleWidth = (image.naturalWidth - faceWidth) * 8;
+        const sample = new Image();
+        let canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, sampleWidth, sampleWidth);
+        sample.src = canvas.toDataURL("image/png", 1.0);
+        // alert(image.naturalWidth + " - "+ faceWidth)
+        return new Promise((resolve, reject) => { resolve(sample); });
+
+        // return sample;
+    }
+
+
+    getAverageRGB =(imgEl) => {
+        var blockSize = 5, // only visit every 5 pixels
+            defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+            canvas = document.createElement('canvas'),
+            context = canvas.getContext && canvas.getContext('2d'),
+            data, width, height,
+            i = -4,
+            length,
+            rgb = {r:0,g:0,b:0},
+            count = 0;
+    
+        if (!context) {
+            return defaultRGB;
+        }
+    
+        height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+        width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+    
+        context.drawImage(imgEl, 0, 0);
+    
+        try {
+            data = context.getImageData(0, 0, width, height);
+        } catch(e) {
+            console.log(e)
+            /* security error, img on diff domain */
+            return defaultRGB;
+        }
+    
+        length = data.data.length;
+    
+        while ( (i += blockSize * 4) < length ) {
+            ++count;
+            rgb.r += data.data[i];
+            rgb.g += data.data[i+1];
+            rgb.b += data.data[i+2];
+        }
+    
+        // ~~ used to floor values
+        rgb.r = ~~(rgb.r/count);
+        rgb.g = ~~(rgb.g/count);
+        rgb.b = ~~(rgb.b/count);
+    
+        return rgb;
     }
 
     runChecks = () => {
         
+        if(!this.bgColorWhiteCheck(this.backgroundColor)){
+            this.status = false;
+            this.message = `Background color must be white!`;
+            return;
+        }
+
+        if(this.input.naturalHeight !== constraints.height && this.input.naturalWidth !== constraints.width){
+            this.status = false;
+            this.message = `Image width and height must be 4inch (384px)`;
+            return;
+        }
+
+
         // Count faces
         const faces = this.fullFaceDescriptions.length; 
         if(!(faces === 1)) {
@@ -146,13 +215,17 @@ class Passport {
             status: this.status || false,
             message: this.message || "Unknown result",
             data: {
-                image: this.image,
+                image:  this.input.src,
                 score: this.detectScore,
-                dimension: this.imageDimension,
+                dimension: {
+                    width: this.input?.naturalWidth,
+                    heigth: this.input?.naturalHeight,
+                },
                 size: 0,
                 faces: this.fullFaceDescriptions?.length,
                 parts: this.faceParts,
-                expression: this.expression
+                expression: this.expression,
+                backgroundColor: this.backgroundColor
             }
         } 
     }
